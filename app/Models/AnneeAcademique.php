2950d2f45e17c5f8289684039efa0c697a445889
mamfaty-sudo <lymamefaty94@gmail.com>
2025-05-13 16:17:30 +0200

    annee academique

--- app/Models/AnneeAcademique.php
+++ app/Models/AnneeAcademique.php
@@ -4,9 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquant\HasMany;
 
 class AnneeAcademique extends Model
 {
     /** @use HasFactory<\Database\Factories\AnneeAcademiqueFactory> */
     use HasFactory;
+
+    protected $table = 'annee_academiques';
+    protected $fillable = ['id', 'date_debut', 'date_fin', 'demarrer'];
+    protected $casts = [
+        'date_debut' => 'date',
+        'date_fin' => 'date',
+        'demarrer' => 'boolean',
+    ];
+
+    public function semestres(): HasMany
+    {
+        return $this->HasMany(Semestre::class);
+    }
+
+    
+
+
+
 }

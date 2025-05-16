a17cb8c903e63935048bb49aaf142019a2516779
mamfaty-sudo <lymamefaty94@gmail.com>
2025-05-16 15:29:08 +0200

    table semestre

--- database/migrations/2025_05_12_215227_create_semestres_table.php
+++ database/migrations/2025_05_12_215227_create_semestres_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->unsignedBigInteger('niveau_id');
             $table->unsignedBigInteger('annee_academique_id');
             $table->string('nom_semestre');
+            $table->interger('nbSemaines');
             $table->timestamps();
             //$table->unsignedBigInteger('annee_academique_id');
 
